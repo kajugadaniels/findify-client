f245e1c3e8423a3163f7e66ba6cd831c637dd662
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-25 19:05:47 +0200

    Add api.js

---
+++ src/api.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const baseURL =
+    import.meta.env.MODE === 'production'
+        ? import.meta.env.VITE_API_BASE_URL_PROD
+        : import.meta.env.VITE_API_BASE_URL;
+
+const apiClient = axios.create({
+    baseURL,
+});
+
+apiClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Token ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+apiClient.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            // Handle invalid token scenario
+            console.error('Unauthorized access. Please log in again.');
+            // Optionally, clear the token and redirect to login page
+            localStorage.removeItem('token');
+            window.location.href = '/';
+        }
+        return Promise.reject(error);
+    }
+);
