5e75335d84887d26ef16030e399e5a1236402b90
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-25 19:31:30 +0200

    New hcanges

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import AppLayout from './layouts/AppLayout'
+import ProtectedRoute from './lib/ProtectedRoute'
 import { Login, NotFound } from './pages'
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<Login />} />
+
+            <Route element={<ProtectedRoute />}>
+                <Route element={<AppLayout />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Route>
+            </Route>
 
             <Route path='*' element={<NotFound />} />
         </Routes>
