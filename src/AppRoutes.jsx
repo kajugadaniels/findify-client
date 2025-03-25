bf749cd67b8604a55d821180eb4f19baf4f35460
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-25 19:26:12 +0200

    Add NotFound page in routes

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Login } from './pages'
+import { Login, NotFound } from './pages'
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<Login />} />
+
+            <Route path='*' element={<NotFound />} />
         </Routes>
     )
 }
