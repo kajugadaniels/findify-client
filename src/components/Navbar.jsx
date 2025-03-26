b8898102ef6e7cf3c50abc49865033d90b55ed38
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-26 13:12:19 +0200

    Design navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Power, Users } from 'lucide-react'
+import { AlignJustify, Bell, Expand, LayoutGrid, Power, Search, Users } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -7,10 +7,18 @@ const Navbar = () => {
         <div className="fixed h-[65px] transition-[margin] duration-100 xl:ml-[275px] group-[.side-menu--collapsed]:xl:ml-[90px] mt-3.5 inset-x-0 top-0 before:content-[''] before:mx-5 before:absolute before:top-0 before:inset-x-0 before:-mt-[15px] before:h-[20px] before:backdrop-blur">
             <div className="box group absolute left-0 right-0 mx-5 h-full transition-colors duration-200 ease-in-out before:absolute before:inset-x-4 before:top-0 before:z-[-1] before:mx-auto before:mt-3 before:h-full before:rounded-lg before:border before:border-slate-200 before:bg-slate-50 before:shadow-sm before:content-[''] before:dark:border-darkmode-500/60 before:dark:bg-darkmode-600/70 xl:left-3.5">
                 <div className="flex h-full w-full items-center px-5 transition-[padding] duration-300 ease-in-out">
+                    <div className="flex items-center gap-1 xl:hidden">
+                        <Link className="open-mobile-menu rounded-full p-2 hover:bg-slate-100" to="#">
+                            <AlignJustify className="stroke-[1] h-[18px] w-[18px]" />
+                        </Link>
+                        <Link className="rounded-full p-2 hover:bg-slate-100" to="#">
+                            <Search className="stroke-[1] h-[18px] w-[18px]" />
+                        </Link>
+                    </div>
                     <nav aria-label="breadcrumb" className="flex hidden flex-1 xl:block">
                         <ol className="flex items-center text-theme-1 dark:text-slate-300">
                             <li className="">
-                                <a href="">Dashboard</a>
+                                <Link to="/dashboard">Dashboard</Link>
                             </li>
                         </ol>
                     </nav>
