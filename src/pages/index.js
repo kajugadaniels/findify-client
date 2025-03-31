ec68cf1f84d67e6b8e0756ab25530111b6095277
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-31 08:13:18 +0200

    Add Users page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,9 @@ export { default as Login } from './auth/Login'
 
 export { default as Dashboard } from './Dashboard'
 
+export { default as GetUsers } from './users/GetUsers'
+export { default as AddUser } from './users/AddUser'
+export { default as UserDetails } from './users/UserDetails'
+export { default as EditUser } from './users/EditUser'
+
 export { default as NotFound } from './NotFound'
